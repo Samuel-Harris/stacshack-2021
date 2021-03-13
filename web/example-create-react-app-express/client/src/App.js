0fd7793b42e6f8cd80a33a984cf7dcbdef8e140c
JoshWheeler08 <josh@joshwheeler.co.uk>
2021-03-13 20:50:07 +0000

    Backend updated

--- web/example-create-react-app-express/client/src/App.js
+++ web/example-create-react-app-express/client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
   
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/data');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     
