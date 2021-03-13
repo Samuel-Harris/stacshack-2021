f15168a8fa4aa99334110ef0ec8c4412fb5e23d3
Samuel-Harris <samuel.matsuo.harris@gmail.com>
2021-03-13 20:25:15 +0000

    fixed comma problem

--- pick_up_line_combiner.py
+++ pick_up_line_combiner.py
@@ -9,7 +9,7 @@ def main():
     for file_name in (chain.from_iterable(glob(os.path.join(x[0], '*.csv')) for x in os.walk('data'))):
         with open(file_name) as csv_file:
             reader = csv.reader(csv_file, delimiter=',')
-            output += '\n'.join(list(reader)[0])
+            output += '\n'.join(list(reader)[0]).replace('"', '')
 
     with open('data.txt', 'w') as f:
         f.write(output)
