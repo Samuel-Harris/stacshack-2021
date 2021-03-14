949bc9dc20458a748ee528af0a0753faa94b7bbd
Daniel Petrov <37422337+daniel-petrov@users.noreply.github.com>
2021-03-14 10:31:33 +0000

    Add files via upload

--- data/main.py
+++ data/main.py
@@ -2,10 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-urls = ["https://pickupline.net/food-pick-up-lines/chipotle-pick-up-lines/", "https://pickupline.net/food-pick-up-lines/subway-pick-up-lines/", "https://pickupline.net/character-based-pick-lines/superheroes-villains-pick-up-lines/"]
+# urls = ["https://pickupline.net/food-pick-up-lines/chipotle-pick-up-lines/", "https://pickupline.net/food-pick-up-lines/subway-pick-up-lines/", "https://pickupline.net/character-based-pick-lines/superheroes-villains-pick-up-lines/"]
 url_base = "https://pickupline.net/"
-category = "food-pick-up-lines/"
-topics = ["chipotle-pick-up-lines/", "subway-pick-up-lines/", "in-n-out-pick-up-lines/"]
+category = "character-based-pick-lines/"
+topics = ["snake-pick-up-lines/","peanuts-pick-up-lines-charlie-brown-and-snoopy/","cow-pick-up-lines/","jurassic-dinosaur-and-prehistoric-caveman-pick-up-lines/",
+          "insect-and-bug-pick-up-lines/","vapire-pick-up-lines/","alien-pick-up-lines/","cat-pick-up-lines/",
+          "animal-pick-up-lines/","superheroes-villains-pick-up-lines/","pirate-pick-up-lines/","elf-pick-up-lines/"]
 
 row = []
 
@@ -16,12 +18,13 @@ for topic in topics:
     soup = BeautifulSoup(page.content, 'html.parser')
     texts = soup.find_all('td', class_='column-1')
     for item in texts:
-        print(item.get_text())
-        text = item.get_text()
-        row.append(item.get_text().replace('"', "'"))
+        text = item.get_text().replace('"', "'")
+        print(text)
+        row.append(text)
     print()
 
-with open('test.csv', 'w', newline='') as file:
+filename = category.replace('/', '.csv')
+print(filename)
+with open(filename, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(row)
-
